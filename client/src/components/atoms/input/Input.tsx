e2cd836a39246740706c11cee90765d777f1286b
Rafael Gomes <rafaelgomes.web@gmail.com>
2020-02-09 18:52:55 -0300

    refactoring fabricant

--- client/src/components/atoms/input/Input.tsx
+++ client/src/components/atoms/input/Input.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Input({...props}) {
     
     return (
-        <input type="text" {...props} />
+        <input {...props} />
     );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,13 +9,15 @@ import 'materialize-css/dist/css/materialize.min.css';
 import MainTemplate from './components/templates/main';
 import CreateChecklist from './components/pages/create-checklist';
 import AdminTemplate from './components/templates/admin';
+import CreateFabricant from './components/pages/create-fabricant';
 
 function App(){
     return (
         <Router>
             <Switch>
                 <MainTemplate path="/" exact component={CreateChecklist} />
-                <AdminTemplate path="/admin" component={CreateChecklist} />
+                <AdminTemplate path="/admin" exact component={CreateChecklist} />
+                <AdminTemplate path="/admin/fabricants/new" exact component={CreateFabricant} />
             </Switch>
         </Router>
     )
